e5bd63ce6967e3be3ab517137d400c259968397a
kevin <124204270+KevinYavari@users.noreply.github.com>
2024-04-30 08:06:54 -0500

    Delete cache directory

--- cache/cache.cpp
+++
@@ -1,7 +0,0 @@
-#include "cache.h"
-
-Cache::Cache() {}
-
-void Cache::escribir(bool valid, string tag, string offset, string missOrHit) {    
-    cache.push_back(Address(valid, tag, offset, missOrHit));
-}
